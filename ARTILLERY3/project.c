9cc9683e6b35c15e12ed283f6925bfe5167c9799
Илья Гончаров <83245775+IlyaGonchar@users.noreply.github.com>
2022-12-13 10:18:31 +0300

    Final c-file

--- ARTILLERY3/project.c
+++ ARTILLERY3/project.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <math.h>   //Для sin, sinf, sinl
+#include <math.h>   //пїЅпїЅпїЅ sin, sinf, sinl
 
 const double PI = 3.141592653589793238463;    //value of pi
 const double G = 9.80665;   //value acceleration of free fall
@@ -58,7 +58,7 @@ int midle_game(){
                 printf("\nERROR--FIRED WRONG WAY, LOSE SHOT.");
             }
             else {
-                srand(time(NULL)); // Новое время для случайного числа
+                srand(time(NULL)); // пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
                 float rand1 = ((float)rand() / (float)(RAND_MAX) * 1.0);
                 dist = sinf(2 * angle / 180 * PI) * speed[nowPlayer] * speed[nowPlayer] / G;
                 erate = field[nowPlayer][nowTarget] / 1000 * rand1;
@@ -92,30 +92,30 @@ int midle_game(){
 }
 
 int start_game(){
-    printf("\nARTILLERY 3");
-    printf("\nMORRISTOWN, NEW JERSEY");
-
-    printf("\nWELCOME TO 'WAR3'. TWO OR THREE HUMANS MAY PLAY!");
-    /*printf("\nDO YOU WISH SOME ASSISTANCE: ");
-
-    scanf("%s", &tempStr);
-    if (tempStr == "YES" || tempStr == "yes" || tempStr == "Yes") {
-        printf("\n\nTHIS IS A WAR GAME. TWO OR THREE PLAYERS ARE GIVEN");
-        printf("\n(THEORETICAL) CANNONS WITH WHICH THEY ATTEMPT TO SHOOT EACH");
-        printf("\nOTHER. THE PARAMETERS FOR DISTANCES AND MUZZLE VELOCITIES ARE");
-        printf("\nSET AT THE BEGINNING OF THE GAME. THE SHOTS ARE FIRED BY");
-        printf("\nGIVING A FIRING ANGLE, EXPRESSED IN DEGREES FROM HORIZONTAL");
-        printf("\n\nTHE COMPUTER WILL KEEP TRACK OF THE GAME AND REPORT ALL");
-        printf("\nMOVES. A 'HIT' IS SCORED BY FIRING A SHOT WITHIN 5% OF THE");
-        printf("\nTOTAL DISTANCE FIRED OVER. GOOD LUCK\n\n");
-    }*/
-
-    printf("\nNUM OF PLAYERS = ");
-    scanf("%d", &numPlayers);
-    if (numPlayers != 2 && numPlayers != 3){
-        printf("ERROR--TWO OR THREE PLAYERS!");
-        return -1;
-    }
+//    printf("\nARTILLERY 3");
+//    printf("\nMORRISTOWN, NEW JERSEY");
+//
+//    printf("\nWELCOME TO 'WAR3'. TWO OR THREE HUMANS MAY PLAY!");
+//    printf("\nDO YOU WISH SOME ASSISTANCE: ");
+//
+//    scanf("%s", &tempStr);
+//    if (tempStr == "YES" || tempStr == "yes" || tempStr == "Yes") {
+//        printf("\n\nTHIS IS A WAR GAME. TWO OR THREE PLAYERS ARE GIVEN");
+//        printf("\n(THEORETICAL) CANNONS WITH WHICH THEY ATTEMPT TO SHOOT EACH");
+//        printf("\nOTHER. THE PARAMETERS FOR DISTANCES AND MUZZLE VELOCITIES ARE");
+//        printf("\nSET AT THE BEGINNING OF THE GAME. THE SHOTS ARE FIRED BY");
+//        printf("\nGIVING A FIRING ANGLE, EXPRESSED IN DEGREES FROM HORIZONTAL");
+//        printf("\n\nTHE COMPUTER WILL KEEP TRACK OF THE GAME AND REPORT ALL");
+//        printf("\nMOVES. A 'HIT' IS SCORED BY FIRING A SHOT WITHIN 5% OF THE");
+//        printf("\nTOTAL DISTANCE FIRED OVER. GOOD LUCK\n\n");
+//    }
+//
+//    printf("\nNUM OF PLAYERS = ");
+//    scanf("%d", &numPlayers);
+//    if (numPlayers != 2 && numPlayers != 3){
+//        printf("ERROR--TWO OR THREE PLAYERS!");
+//        return -1;
+//    }
 
 
     // 290-440
@@ -142,7 +142,23 @@ int start_game(){
 int main() { // (10 - 20) 
     printf("                    BINARY");
     printf("\n  CREATIVE COMPUTING  MORRISTOWN NEW JERSEY\n");
+    printf("\nARTILLERY 3");
+    printf("\nMORRISTOWN, NEW JERSEY");
 
+    printf("\nWELCOME TO 'WAR3'. TWO OR THREE HUMANS MAY PLAY!");
+    printf("\nDO YOU WISH SOME ASSISTANCE: ");
+
+    scanf("%s", &tempStr);
+    if (tempStr == "YES" || tempStr == "yes" || tempStr == "Yes") {
+        printf("\n\nTHIS IS A WAR GAME. TWO OR THREE PLAYERS ARE GIVEN");
+        printf("\n(THEORETICAL) CANNONS WITH WHICH THEY ATTEMPT TO SHOOT EACH");
+        printf("\nOTHER. THE PARAMETERS FOR DISTANCES AND MUZZLE VELOCITIES ARE");
+        printf("\nSET AT THE BEGINNING OF THE GAME. THE SHOTS ARE FIRED BY");
+        printf("\nGIVING A FIRING ANGLE, EXPRESSED IN DEGREES FROM HORIZONTAL");
+        printf("\n\nTHE COMPUTER WILL KEEP TRACK OF THE GAME AND REPORT ALL");
+        printf("\nMOVES. A 'HIT' IS SCORED BY FIRING A SHOT WITHIN 5% OF THE");
+        printf("\nTOTAL DISTANCE FIRED OVER. GOOD LUCK\n\n");
+    }
     start_game();
     midle_game();
     printf("\nGAME OVER. PLAYER#%d WINS.", nowPlayer);
